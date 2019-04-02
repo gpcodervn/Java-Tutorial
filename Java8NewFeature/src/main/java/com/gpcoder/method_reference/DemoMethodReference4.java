63b785e5e1506baf5650a40463e7e881eb659d10
Giang Phan <ptgiang56@gmail.com>
2019-04-02 20:11:56 +0700

    Constructor reference example

--- Java8NewFeature/src/main/java/com/gpcoder/method_reference/DemoMethodReference4.java
+++ Java8NewFeature/src/main/java/com/gpcoder/method_reference/DemoMethodReference4.java
@@ -5,9 +5,14 @@ interface SayHello {
 	void display(String say);
 }
 
-class Hello {
+class Hello implements SayHello {
 	public Hello(String say) {
 		System.out.print(say);
+	}
+
+	@Override
+	public void display(String say) {
+		System.out.println(say);
 	}
 }
 
