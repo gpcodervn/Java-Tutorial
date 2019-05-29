acce2db7551918a93b7de0ca1dc3f0d0b4512565
Giang Phan <ptgiang56@gmail.com>
2019-05-30 00:38:27 +0700

    Java Web Services – Jersey JAX-RS – REST - CRUD exmaple

---
+++ RestfulWebServiceExample/src/com/gpcoder/User.java
@@ -0,0 +1,17 @@
+package com.gpcoder;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@XmlRootElement(name = "user")
+public class User {
+
+	private Integer id;
+	private String username;
+}
