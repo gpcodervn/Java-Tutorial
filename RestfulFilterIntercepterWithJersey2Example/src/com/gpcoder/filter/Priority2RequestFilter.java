43200733a9ce4cf1f635e711fc3446d83c7e817e
Giang Phan <ptgiang56@gmail.com>
2019-06-16 22:52:52 +0700

    Update ContainerRequestFilter

--- RestfulFilterIntercepterWithJersey2Example/src/com/gpcoder/filter/Priority2RequestFilter.java
+++ RestfulFilterIntercepterWithJersey2Example/src/com/gpcoder/filter/Priority2RequestFilter.java
@@ -3,16 +3,16 @@ package com.gpcoder.filter;
 import java.io.IOException;
 
 import javax.annotation.Priority;
-import javax.ws.rs.client.ClientRequestContext;
-import javax.ws.rs.client.ClientRequestFilter;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.ext.Provider;
 
 @Priority(20)
 @Provider
-public class Priority2RequestFilter implements ClientRequestFilter {
+public class Priority2RequestFilter implements ContainerRequestFilter {
 
 	@Override
-	public void filter(ClientRequestContext requestContext) throws IOException {
+	public void filter(ContainerRequestContext requestContext) throws IOException {
 		System.out.println("#2 Priority2RequestFilter running ... ");
 	}
 }
